b76aae45327cd67ff05a43f4dd0f25580cc483da
Muhammed Mukthar <muktharmanaf28@gmail.com>
2022-10-29 14:31:38 +0530

    added user schema validation

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -1,5 +1,22 @@
 
-import {object} from 'zod'
+import {object,string} from 'zod'
 export const createUser =object({
-    
+
+    body:object({
+        name:string({
+            required_error:'Name is required'
+        }),
+        password:string({
+            required_error:'password is required'
+        }).min(6,'password is too short'),
+        passwordConfirmation:string({
+            required_error:'password is required'
+        }).min(6,'passwordConfirm is too short'),
+        email:string({
+            required_error:"Email is required"
+        }).email('Not a valid email')
+}).refine((data)=>data.password === data.passwordConfirmation,{
+    message:"password do not match",
+    path:['passwordConfirmation']
+})
 })
